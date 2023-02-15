3dc03365bc6daa3444e5d3b69773229822204db7
mop <marcpenn@hotmail.com>
2023-02-15 17:16:33 -0500

    Scroll reveal effect

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 }
+
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.hero-text', {delay:200, origin:'top'});
